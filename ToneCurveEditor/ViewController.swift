cee4fe68f9c9d3ec54e4be7c63c892b36125ba79
Simon Gladman <sgladman@Simon-Gladmans-MacBook-Pro.local>
2014-09-14 07:44:43 +0100

    Added reset curve button and implemented didSet observer on view controller's curveValues property

--- ToneCurveEditor/ViewController.swift
+++ ToneCurveEditor/ViewController.swift
@@ -12,8 +12,9 @@ class ViewController: UIViewController
 {
     let imageWidget = ImageWidget(frame: CGRectZero)
     let toneCurveEditor = ToneCurveEditor(frame: CGRectZero)
+    let resetButton = UIButton(frame: CGRectZero)
 
-    var curveValues = [0.0, 0.25, 0.5, 0.75, 1.0]
+    
     
     override func viewDidLoad()
     {
@@ -21,18 +22,35 @@ class ViewController: UIViewController
 
         imageWidget.viewController = self
         
+        resetButton.setTitle("Reset Curve", forState: .Normal)
+        resetButton.setTitleColor(UIColor.blackColor(), forState: .Normal)
+        resetButton.addTarget(self, action: "resetButtonClickedSelector:", forControlEvents: .TouchUpInside)
+        
         view.addSubview(imageWidget)
         view.addSubview(toneCurveEditor)
+        view.addSubview(resetButton)
         
         toneCurveEditor.curveValues = curveValues
         toneCurveEditor.addTarget(self, action: "toneCurveEditorChangedSelector:", forControlEvents: .ValueChanged)
     }
     
+    func resetButtonClickedSelector(value: UIButton)
+    {
+        curveValues = [0.0, 0.25, 0.5, 0.75, 1.0]
+    }
+    
     func toneCurveEditorChangedSelector(value : ToneCurveEditor)
     {
         curveValues = value.curveValues
-        
-        imageWidget.curveValues = curveValues
+    }
+    
+    var curveValues: [Double] = [0.0, 0.25, 0.5, 0.75, 1.0]
+    {
+        didSet
+        {
+            imageWidget.curveValues = curveValues
+            toneCurveEditor.curveValues = curveValues
+        }
     }
     
     override func viewDidLayoutSubviews()
@@ -46,7 +64,9 @@ class ViewController: UIViewController
             let widgetHeight = Int(view.frame.size.height) / 2
             
             imageWidget.frame = CGRect(x: 5, y: topMargin, width: widgetWidth - 10, height: widgetHeight - topMargin - topMargin)
-            toneCurveEditor.frame = CGRect(x: 0, y: widgetHeight, width: widgetWidth, height: widgetHeight)
+            toneCurveEditor.frame = CGRect(x: 0, y: widgetHeight, width: widgetWidth, height: widgetHeight - 50)
+            
+            resetButton.frame = CGRect(x: 20, y: view.frame.height - 70, width: 100, height: 50)
         }
         else
         {
@@ -55,7 +75,9 @@ class ViewController: UIViewController
             let widgetHeight = Int(view.frame.size.height)
             
             imageWidget.frame = CGRect(x: widgetWidth, y: topMargin, width: widgetWidth - 5, height: widgetHeight - topMargin - topMargin)
-            toneCurveEditor.frame = CGRect(x: 0, y: 0, width: widgetWidth, height: widgetHeight)
+            toneCurveEditor.frame = CGRect(x: 0, y: 0, width: widgetWidth, height: widgetHeight - 50)
+            
+            resetButton.frame = CGRect(x: 20, y: view.frame.height - 70, width: 100, height: 50)
         }
     }
 
